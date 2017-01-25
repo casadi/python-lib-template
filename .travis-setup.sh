c2516ad8459a15a1886191245a06be4b0fed1b4f
jgillis <joris.gillis42@gmail.com>
2017-01-25 10:32:15 +0100

    Update .travis-setup.sh

--- .travis-setup.sh
+++ .travis-setup.sh
@@ -132,5 +132,5 @@ echo "/opt/wine-staging/bin/wine $EASY_INSTALL" '$@' > _easy_install
 echo "/opt/wine-staging/bin/wine $PIP" '$@' > _pip
 chmod +x _python _easy_install _pip
 
-wget https://bootstrap.pypa.io/ez_setup.py -O - | sed "s/DEFAULT_VERSION = LATEST/DEFAULT_VERSION = '24.3.0'/g" | ./_python
+wget https://bootstrap.pypa.io/ez_setup.py -O - | sed "s/DEFAULT_VERSION = .*/DEFAULT_VERSION = '24.3.0'/g" | ./_python
 ./_easy_install pip
