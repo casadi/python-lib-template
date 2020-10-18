fbf433343b83bbffcad0c8a765596b8e6a47eb23
jgillis <joris.gillis42@gmail.com>
2020-10-18 14:06:06 +0200

    Update .travis-setup.sh

--- .travis-setup.sh
+++ .travis-setup.sh
@@ -201,7 +201,13 @@ EOF
         https://www.python.org/ftp/python/$VERSION/win32/tools.msi
         https://www.python.org/ftp/python/$VERSION/win32/pip.msi
 EOF
-
+        cat install_list.txt
+        INSTALLER_URL="-i install_list.txt"
+        INSTALL_COMMAND="wget $WINETRICKS;chmod +x winetricks;Xvfb :1& export DISPLAY=:1;./winetricks nocrashdialog -q vcrun2017;pkill Xvfb;export DISPLAY=;wine msiexec /i core.msi TARGETDIR=C:\Python$VERSIONSHORT;wine msiexec /i dev.msi TARGETDIR=C:\Python$VERSIONSHORT;wine msiexec /i lib.msi TARGETDIR=C:\Python$VERSIONSHORT;wine msiexec /i exe.msi TARGETDIR=C:\Python$VERSIONSHORT;wine msiexec /i tools.msi TARGETDIR=C:\Python$VERSIONSHORT;wine msiexec /i pip.msi TARGETDIR=C:\Python$VERSIONSHORT;"
+        CLEAN_COMMAND=""
+        EXECDIR="$HOME/.wine/drive_c/Python$VERSIONSHORT"
+        MORE_COMMANDS= # Needs VC++ 10.0
+        ;;
     py39_64)
         VERSION=3.9.0
         VERSIONSHORT=39
